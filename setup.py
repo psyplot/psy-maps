869313e1bc6f6e8dde2e7ddaeaae894ea24e3a28
Chilipp <p.sommer@posteo.de>
2017-04-03 17:18:47 +0200

    Revert "Bug fix to allow the script to be run from outside psy-simple"
    This reverts commit 949dc667f8c6987796577a2cb03ff945957a7789.

--- setup.py
+++ setup.py
@@ -4,14 +4,8 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 
-setup_dir = osp.dirname(__file__)
-
-# is set down below
-__version__ = None
-
-
 def readme():
-    with open(osp.join(setup_dir, 'README.rst')) as f:
+    with open('README.rst') as f:
         return f.read()
 
 
@@ -31,7 +25,7 @@ class PyTest(TestCommand):
 
 
 # read the version from version.py
-with open(osp.join(setup_dir, 'psy_maps', 'version.py')) as f:
+with open(osp.join('psy_maps', 'version.py')) as f:
     exec(f.read())
 
 
